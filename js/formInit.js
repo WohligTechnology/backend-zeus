ff637038cbc80c647e87837a7fce5ab8eb65d8d2
jayvisariya93 <jayvisariya93@gmail.com>
2015-09-16 20:24:29 +0530

    Material UI Backend done

---
+++ js/formInit.js
@@ -0,0 +1,22 @@
+$('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+  });
+
+$(document).ready(function() {
+    $('select').material_select();
+  });
+
+var slider = document.getElementById('test52');
+  noUiSlider.create(slider, {
+   start: [20, 80],
+   connect: true,
+   step: 1,
+   range: {
+     'min': 0,
+     'max': 100
+   },
+   format: wNumb({
+     decimals: 0
+   })
+  });
